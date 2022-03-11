fb8c02d2997e2dff46806667f1823bbaaa3b0f8c
Yoseph.Gashawtena <jossy.gashaw@gmail.com>
2022-03-10 18:41:23 -0800

    Working with printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  *main - Entry point
  *
- *Return : 0 if the program is successful
+ *Return : Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf ("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
