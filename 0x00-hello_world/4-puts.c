2008f4105044c7f3e45f1232c37587c43de7dba3
Yoseph.Gashawtena <jossy.gashaw@gmail.com>
2022-03-10 16:34:45 -0800

    Working with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc hello.c
+gcc -E hello.c
--- 0x00-hello_world/hello.c
+++ 0x00-hello_world/hello.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-#include<string.h>
+/**
+ *
+ *main - Entry point
+ *
+ *Return : Always 0 (Success)
+ */
 int main(void)
 {
-	char str1[] ="Programming is like building a multilingual puzzle";
-	puts(str1);
+  puts("\"Programming is like building a multilingual puzzle");
+  Return (0);
 }
